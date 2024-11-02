4259608b079b3507d3c22cead8cb379b40a40cda
Marcelo Melo <marcelovmelo201@gmail.com>
2024-11-02 18:57:41 -0300

    commit

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,78 @@
+package application;
+
+import entities.BankAccount;
+
+import java.text.ParseException;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws ParseException {
+
+        Scanner sc = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
+
+        double amount = 0;
+        boolean escolha = true;
+
+        System.out.print("Digite o número da conta: ");
+        int number = sc.nextInt();
+
+        System.out.print("Digite o nome do titular: ");
+        String name = scan.nextLine();
+
+        System.out.print("Digite o montante inicial: ");
+        double balance = sc.nextDouble();
+
+        BankAccount bankAccount = new BankAccount(number, name, balance);
+
+        System.out.println();
+
+        System.out.println(bankAccount);
+
+        while(escolha){
+            System.out.println();
+
+            System.out.println("1 - Saque\n2 - Depósito\n3 - Listar transações\n4 - sair\n");
+            int num = sc.nextInt();
+
+            switch (num){
+
+                case 1:
+                    System.out.print("Digite o valor a ser sacado: ");
+                    amount = sc.nextDouble();
+
+                    bankAccount.withdraw(amount);
+                    break;
+
+                case 2:
+                    System.out.printf("Digite o valor a ser depositado: ");
+                    amount = sc.nextDouble();
+
+                    bankAccount.deposit(name, amount);
+                    break;
+
+                case 3:
+                    bankAccount.transactions();
+                    break;
+
+                case 4:
+                    escolha = false;
+                    break;
+
+
+                default:
+                    System.out.printf("Opção inválida.");
+            }
+        }
+
+        System.out.printf("Saldo: R$ " + bankAccount.getBalance());
+
+        System.out.println(bankAccount);
+
+
+        System.out.printf("Saldo: R$ " + bankAccount.getBalance());
+
+        sc.close();
+        scan.close();
     }
 }
--- src/entities/BankAccount.java
+++ src/entities/BankAccount.java
@@ -1,2 +1,93 @@
-package entities;public class BankAccount {
+package entities;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class BankAccount {
+
+    private int number;
+    private String name;
+    private double balance;
+    private List<Double> transactions = new ArrayList<>();
+    private List<Date> transactionsDates = new ArrayList<>();
+    private List<String> transactionsNames = new ArrayList<>();
+
+
+
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+    public BankAccount(){}
+
+    public BankAccount(int number, String cardholderName, double balance) {
+        this.number = number;
+        this.name = cardholderName;
+        this.balance = balance;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+   public void deposit(String cardholderName, double amount){
+
+        if(cardholderName.equals(this.name)){
+            this.balance += amount;
+            Date now = new Date();
+            transactionsDates.add(now);
+            transactionsNames.add("Depósito");
+        }
+        else {
+            System.out.println("Nome de titular não encontrado. Operação cancelada");
+        }
+
+   }
+
+   public void withdraw(double amount){
+        if(balance - amount >= 0){
+            balance -= amount;
+            transactions.add(amount);
+
+            Date now = new Date();
+
+            transactionsDates.add(now);
+            transactionsNames.add("Saque");
+        }
+        else {
+            System.out.println("Operação cancelada. O montante não pode ser maior que o saldo em conta!");
+        }
+   }
+
+   public void transactions() throws ParseException {
+
+       System.out.println(" === Transação, Valor, Data ===");
+        for(int i = 0; i < transactions.size(); i++){
+            System.out.println(transactionsNames.get(i) + ": R$ " + transactions.get(i) + ", " + transactionsDates);
+
+        }
+   }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(number);
+        sb.append(", ");
+        sb.append(name);
+        sb.append(", R$ ");
+        sb.append(balance);
+
+        return sb.toString();
+    }
+
+
 }
